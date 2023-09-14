ae779cc90ee88b0216a0fa18ae5fea6cd51ff4e1
Igor Volkov <igorvolkov842@gmail.com>
2023-09-14 10:38:16 +0400

    ком

--- Website/Areas/Admin/Controllers/HomeController.cs
+++ Website/Areas/Admin/Controllers/HomeController.cs
@@ -7,31 +7,9 @@ namespace Website.Areas.Admin.Controllers
     [Area("Admin")]
     public class HomeController : Controller
     {
-   
-
         public IActionResult Index()
         {
             return View();
         }
-
-        //[HttpGet]
-        //public async Task Image()
-        //{
-        //    string content = @"<form method='post' enctype='multipart/form-data'>" +
-        //        "<input type='file' name='file' /><br />" +
-        //        "<input type='submit' value='Send' />" +
-        //        "</form>";
-        //    Response.ContentType = "text/html;charset=utf-8";
-        //    await Response.WriteAsync(content);
-        //}
-
-        //[HttpPost]
-        //public void Image(IFormFile file)
-        //{
-        //    using (var stream = new FileStream(@"C:\Users\User1\Desktop\image.jpg", FileMode.Create))
-        //    {
-        //        file.CopyTo(stream);
-        //    }
-        //}
     }
 }
